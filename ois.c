8895540760060920ca6e830b029a08454fd87d29
Forest Belton <fbelton@theore.tc>
2013-05-29 21:34:11 -0700

    Add newline after printing output

--- ois.c
+++ ois.c
@@ -6,5 +6,5 @@ int main() {
   while(m - mem < sizeof mem && fread(++m, 1, 1, stdin));
   while(p = ((mem[mem[p+1]] -= mem[mem[p]]) <= 0) ? mem[p+2] : p+3);
 
-  return putchar(mem[0]), 0;
+  return printf("%c\n", mem[0]), 0;
 }

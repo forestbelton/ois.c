5214077aecbaeb790673f6558422ac0168dfb0fe
Forest Belton <fbelton@theore.tc>
2013-05-29 21:34:02 -0700

    Fix off-by-one and indirection bugs

--- ois.c
+++ ois.c
@@ -4,7 +4,7 @@ int main() {
   unsigned char mem[256] = {0}, p = 1, *m = &mem[0];
 
   while(m - mem < sizeof mem && fread(++m, 1, 1, stdin));
-  while(p = ((mem[p+2] -= mem[p+1]) <= 0) ? mem[p+3] : p+3);
+  while(p = ((mem[mem[p+1]] -= mem[mem[p]]) <= 0) ? mem[p+2] : p+3);
 
   return putchar(mem[0]), 0;
 }

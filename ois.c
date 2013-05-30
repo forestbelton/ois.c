4d9e87f71601a2aab04f8ae3617d0458098f7b10
Forest Belton <fbelton@theore.tc>
2013-05-30 01:59:32 -0700

    Small changes to quiet GCC warnings

--- ois.c
+++ ois.c
@@ -3,8 +3,8 @@
 int main() {
   unsigned char mem[256] = {0}, p = 1, *m = &mem[0];
 
-  while(m - mem < sizeof mem && fread(++m, 1, 1, stdin));
-  while(p = ((mem[mem[p+1]] -= mem[mem[p]]) <= 0) ? mem[p+2] : p+3);
+  while((size_t)(m - mem) < sizeof mem && fread(++m, 1, 1, stdin));
+  while((p = (mem[mem[p+1]] -= mem[mem[p]]) <= 0 ? mem[p+2] : p+3));
 
   return printf("%c\n", mem[0]), 0;
 }

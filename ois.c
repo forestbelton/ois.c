9460c604be7930a0af298eefc6936af4beb4362b
Forest Belton <fbelton@theore.tc>
2013-05-29 20:35:12 -0700

    Use simpler array size

--- ois.c
+++ ois.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  unsigned char mem[(1 << 8) + 1] = {0}, p = 1, *m = &mem[0];
+  unsigned char mem[256] = {0}, p = 1, *m = &mem[0];
 
   while(m - mem < sizeof mem && fread(++m, 1, 1, stdin));
   while(p = ((mem[p+2] -= mem[p+1]) <= 0) ? mem[p+3] : p+3);

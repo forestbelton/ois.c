be2f8e6f5fcd4359d2bbdabad389b384635d0282
Forest Belton <fbelton@theore.tc>
2013-05-29 18:41:45 -0700

    First version of interpreter

---
+++ ois.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  unsigned char mem[(1 << 8) + 1] = {0}, p = 1, *m = &mem[0];
+
+  while(m - mem < sizeof mem && fread(++m, 1, 1, stdin));
+  while(p = ((mem[p+2] -= mem[p+1]) <= 0) ? mem[p+3] : p+3);
+
+  return putchar(p[0]), 0;
+}
